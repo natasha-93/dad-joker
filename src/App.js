898aa215eab8c71683342854bea76c7dca8b8733
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-09 20:13:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [joke, setJoke] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={e => {
+        fetch('https://icanhazdadjoke.com/', {
+          headers: {
+            'Accept': 'application/json'
+          },
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            setJoke(data.joke)
+          });
+      }}>Click Me!</button>
+      <p>{joke}</p>
     </div>
   );
 }
 
 export default App;
+
