b26af242e493786c795c7886cf1230f73dff05a6
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-09 20:22:50 -0600

    Add loading state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 function App() {
   const [joke, setJoke] = useState("")
+  const [loading, setLoading] = useState(false)
   return (
     <div>
       <button onClick={e => {
+        setLoading(true)
         fetch('https://icanhazdadjoke.com/', {
           headers: {
             'Accept': 'application/json'
@@ -15,9 +17,11 @@ function App() {
           })
           .then((data) => {
             setJoke(data.joke)
+            setLoading(false)
           });
-      }}>Click Me!</button>
-      <p>{joke}</p>
+      }}>
+        Click Me!</button>
+      <p>{loading ? "loading" : joke}</p>
     </div>
   );
 }
